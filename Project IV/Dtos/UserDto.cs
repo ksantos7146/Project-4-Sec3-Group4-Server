c5504a081eca9ac796f2a5b3a17f2234a0626d91
Dzhanybek Zakiriiaev <dzhanybek.zakiriiaev@gmail.com>
2025-04-04 16:16:58 -0400

    quizk fix

--- Project IV/Dtos/UserDto.cs
+++ Project IV/Dtos/UserDto.cs
@@ -6,6 +6,7 @@ namespace Project_IV.Dtos
     {
         public string UserId { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string? Bio { get; set; }
         public int? GenderId { get; set; }
--- Project IV/Mappers/UserMapper.cs
+++ Project IV/Mappers/UserMapper.cs
@@ -13,6 +13,7 @@ namespace Project_IV.Mappers
                 Username = user.UserName,
                 Bio = user.Bio,
                 GenderId = user.GenderId,
+                Email = user.Email,
                 StateId = user.StateId,
                 Age = user.Age,
                 Images = user.Images.Select(i => i.ToDto()).ToList()
@@ -27,6 +28,7 @@ namespace Project_IV.Mappers
                 Bio = userDto.Bio,
                 GenderId = userDto.GenderId,
                 StateId = userDto.StateId,
+                Email= userDto.Email,
                 Age = userDto.Age,
                 Images = userDto.Images.Select(i => i.ToEntity()).ToList()
             };
